7e2c5949d027015a6cb1a75fff706dce0edeafc4
Sourabh Dubey <33506407+souru7@users.noreply.github.com>
2022-11-01 18:11:36 +0530

    main branch

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,22 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audioplayers_web/audioplayers_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:wakelock_web/wakelock_web.dart';
+import 'package:zego_express_engine/zego_express_engine_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioplayersPlugin.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  WakelockWeb.registerWith(registrar);
+  ZegoExpressEngineWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
